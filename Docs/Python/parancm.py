4dba47f803f85be5b536e17fa7ddb8cbac93ea6d
Gino <69703617+pamosa@users.noreply.github.com>
2025-10-15 12:50:14 -0300

    Update parancm.py

--- Docs/Python/parancm.py
+++ Docs/Python/parancm.py
@@ -9,29 +9,37 @@ def barra_progresso(etapa_atual, total_etapas, descricao="", largura=50):
     if etapa_atual == total_etapas:
         print()
 
-origem = os.path.expanduser("~/Downloads/Python/")
+origem = os.path.expanduser("~/Downloads/Python/arquivos/")
+codigos = os.path.expanduser("~/Downloads/Python/codigos/")
+destino = os.path.expanduser("~/Downloads/Python/")
 
-arquivo_1 = origem + 'EXP.csv'
-arquivo_2 = origem + 'IMP.csv'
-ncm = origem + 'NCM.csv'
-pais = origem + 'pais.csv'
-estados = origem + 'estados.csv'
-municipios = origem + 'municipios.csv'
-via = origem + 'VIA.csv'
-urf = origem + 'URF.csv'
+arquivo_1 = origem + 'EXP_2023.csv'
+arquivo_2 = origem + 'EXP_2024.csv'
+arquivo_3 = origem + 'EXP_2025.csv'
+arquivo_4 = origem + 'IMP_2023.csv'
+arquivo_5 = origem + 'IMP_2024.csv'
+arquivo_6 = origem + 'IMP_2025.csv'
+ncm = codigos + 'NCM.csv'
+pais = codigos + 'pais.csv'
+estados = codigos + 'estados.csv'
+municipios = codigos + 'municipios.csv'
+via = codigos + 'VIA.csv'
+urf = codigos + 'URF.csv'
 
-finalexp = pd.read_csv(arquivo_1,low_memory=False,sep=';',encoding='UTF-8')
-finalimp = pd.read_csv(arquivo_2,low_memory=False,sep=';',encoding='UTF-8')
+finalexp2023 = pd.read_csv(arquivo_1,low_memory=False,sep=';',encoding='UTF-8')
+finalexp2024 = pd.read_csv(arquivo_2,low_memory=False,sep=';',encoding='UTF-8')
+finalexp2025 = pd.read_csv(arquivo_3,low_memory=False,sep=';',encoding='UTF-8')
+finalimp2023 = pd.read_csv(arquivo_4,low_memory=False,sep=';',encoding='UTF-8')
+finalimp2024 = pd.read_csv(arquivo_5,low_memory=False,sep=';',encoding='UTF-8')
+finalimp2025 = pd.read_csv(arquivo_6,low_memory=False,sep=';',encoding='UTF-8')
 
 finalncm = pd.read_csv(ncm,low_memory=False,sep=';',encoding='latin1')
 finalpais = pd.read_csv(pais,low_memory=False,sep=';',encoding='latin1')
-finalestados = pd.read_csv(estados,low_memory=False,sep=';',encoding='latin1')
-finalmunicipios = pd.read_csv(municipios,low_memory=False,sep=';',encoding='latin1')
 finalvia = pd.read_csv(via,low_memory=False,sep=';',encoding='latin1')
 finalurf = pd.read_csv(urf,low_memory=False,sep=';',encoding='latin1')
 
-exp_finalexp = pd.concat([finalexp], ignore_index=True)
-exp_finalimp = pd.concat([finalimp], ignore_index=True)
+exp_finalexp = pd.concat([finalexp2023, finalexp2024, finalexp2025], ignore_index=True)
+exp_finalimp = pd.concat([finalimp2023, finalimp2024, finalimp2025], ignore_index=True)
 
 def processar(df):
     df = df.merge(finalpais[['CO_PAIS', 'NO_PAIS_ING']], on='CO_PAIS', how='left')
@@ -67,13 +75,13 @@ exp_finalexp = processar(exp_finalexp)
 exp_finalimp = processar(exp_finalimp)
 
 exp_finalexp.to_csv(
-    origem + 'exp_finalexp.csv', 
+    destino + 'exp_finalexp.csv', 
     index=False, 
     sep=';', 
     encoding='utf-8-sig')
 
 exp_finalimp.to_csv(
-    origem + 'exp_finalimp.csv', 
+    destino + 'exp_finalimp.csv', 
     index=False, 
     sep=';', 
     encoding='utf-8-sig')
